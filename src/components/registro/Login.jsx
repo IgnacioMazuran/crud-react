0e592e1adc4a41fc55099c9f4d1f86ae21f92a35
Ignacio Mazuran <ignaciomazuran12@gmail.com>
2021-05-15 12:59:13 -0300

    add cerrar sesion e iniciar botones

--- src/components/registro/Login.jsx
+++ src/components/registro/Login.jsx
@@ -1,18 +1,40 @@
 import React,{useState} from 'react';
+import {useHistory } from 'react-router-dom';
 import {auth} from '../../firebaseReg';
 const Login = () => {
-
+    const historial = useHistory();
     const [email,setEmail] = useState('');
     const [pw,setPw] = useState('');
+    const [msgerror,setMsgError] = useState('');
 
     const registro = async e => {
         e.preventDefault()
-        try{
-            await auth.createUserWithEmailAndPassword(email,pw);
-            alert('Usuario registrado con Éxito');
-        } catch(e) {
-            console.log(e);
-        }
+        await auth.createUserWithEmailAndPassword(email,pw)
+        .then(r =>{ 
+            historial.push('/');
+            alert('Logeado con éxito')
+        })
+        .catch(e => {
+            if(e.code === 'auth/invalid-email'){
+                setMsgError('Email incorrecto');
+            }
+            if(e.code === 'auth/weak-password'){
+                setMsgError('La contraseña es débil: debe tener 6 o más caracteres');
+            }
+        }) 
+    }
+
+    const loginUser = () => {
+        auth.signInWithEmailAndPassword(email,pw)
+        .then(r =>{ 
+            historial.push('/');
+            alert('Logeado con éxito')
+        })
+        .catch((err) =>{ 
+            if(err.code === 'auth/wrong-password'){
+                setMsgError('Password incorrecta');
+            }
+        })
     }
 
     return (
@@ -28,8 +50,17 @@ const Login = () => {
                         className='form-control'
                         placeholder='Introduce tu contraseña'/>
                          <button type="button" onClick={registro} className="btn btn-dark col-12 mt-4">Registrar Usuario</button>
+                         <button type="button" onClick={loginUser} className="btn btn-primary col-12 mt-2">Iniciar Sesión</button>
+
                     </form>
-                
+                    
+                {
+                    msgerror != null ? (
+                        <div className='mt-3'>{msgerror}</div>
+                    ) : (
+                        <span></span>
+                    )
+                }
             </div>
             <div className='col'></div>
         </div>
--- src/components/registro/Menu.jsx
+++ src/components/registro/Menu.jsx
@@ -1,22 +1,80 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { auth } from '../../firebaseReg';
+import { useHistory } from 'react-router-dom';
+
+
 const Menu = () => {
+    const historial = useHistory();
+    const [usuario, setUsuario] = useState(null);
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                setUsuario(user.email);
+            }
+        })
+    }, [])
+
+    const cerrarSesion = () => {
+        auth.signOut();
+        setUsuario(null);
+        historial.push('/');
+
+    }
+
     return (
-        <div className='mw-100'>
-            <nav className='navbar navbar-expand-lg navbar-dark bg-dark p-3 mw-200 ms-auto ' >
+        <div>
+
+            <nav className='navbar navbar-expand-lg navbar-dark bg-dark p-1 ' >
                 <ul className='navbar-nav mr-auto'>
                     <li className='nav-item'>
-                    <Link className='nav-link' to='/'>Inicio</Link>
+                        <Link className='nav-link' to='/'>Inicio</Link>
+                    </li>
+                    <li className='nav-item'>{
+                        !usuario ? (
+                            <Link className='nav-link' to='/admin'>Admin</Link>
+
+                        ) : (
+                            <span></span>
+                        )}
                     </li>
                     <li className='nav-item'>
-                    <Link className='nav-link' to='/admin'>Admin</Link>
-                    </li>
-                    <li className='nav-item'>
-                    <Link className='nav-link' to='/login'>Login</Link>
+                        {
+                            !usuario ? (
+                                <Link className='nav-link' to='/login'>Login</Link>
+
+                            ) : (
+                                <span></span>
+                            )
+                        }
                     </li>
                 </ul>
+
+
+                <form className="container-fluid">
+                </form>
+
+                <nav class="navbar navbar-light bg-light">
+                    <div class="container-fluid">
+                        <form class="d-flex">{
+                            usuario ? (
+                                <button onClick={cerrarSesion} className='btn btn-danger float-right'>Cerrar Sesión</button>
+                            ) :
+                                (
+                                    <span></span>
+                                )
+                        }
+
+
+                        </form>
+                    </div>
+                </nav>
             </nav>
+
+
         </div>
+
+
     );
 };
 
